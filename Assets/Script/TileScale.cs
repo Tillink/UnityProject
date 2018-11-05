bd1393a2fc1eecf47d926c9dfcceae1a62a25780
dkaneorkfl <41276468+dkaneorkfl@users.noreply.github.com>
2018-11-05 19:24:24 +0900

    Add files via upload

---
+++ Assets/Script/TileScale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScale : MonoBehaviour {
+
+	public float tileSize;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Material>().mainTextureScale = new Vector2(transform.localScale.x*tileSize,transform.localScale.z*tileSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
