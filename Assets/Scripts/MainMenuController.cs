adc46e20cb7c0f4a17efc1262b8979d91b31ccac
query1280 <41275409+query1280@users.noreply.github.com>
2018-11-06 16:01:49 +0900

    1106오후4시
    메뉴 추가

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class MainMenuController : MonoBehaviourEx
+{
+	public void Btn_Start(){//메인메뉴
+		Debug.Log("1");
+		SceneManager.LoadScene("Menu_start_difficulties");
+	}
+
+	public void Btn_Score(){//점수메뉴
+		Debug.Log("2");
+		SceneManager.LoadScene("Menu_score_chart");
+	}
+	public void Btn_Quit(){//종료
+		Debug.Log("3");
+		Application.Quit();
+	}
+	public void Btn_Tomenu(){//메뉴로 돌아가기
+		Debug.Log("4");
+		SceneManager.LoadScene("Menu_main");
+	}
+	public void Btn_escape(){//esc팝업메뉴
+		Debug.Log("5");
+		if(Input.GetKeyDown(KeyCode.Escape))
+			SceneManager.LoadScene("Menu_popup_whilePlaying");
+	}
+}
