0c847950c84bc110d0014ba6ec77220e87cd3578
query1280 <41275409+query1280@users.noreply.github.com>
2018-11-07 21:48:48 +0900

    폭탄심지 애니메이션
    폭탄심지애니

---
+++ Assets/Scripts/BombTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class BombTime : MonoBehaviour {
+
+	public GameObject bomb;
+	
+	public float _decreaseCount = 0.01f;
+	public void Update(){
+			bomb.GetComponent<Image>().fillAmount -= _decreaseCount;
+
+	}
+}
